359f38df1f14aa2fbf7bb7f48da17e5c90763fca
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-07 16:34:20 -0400

    Fix nil ship address error.

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -5,6 +5,6 @@ Spree::LineItem.class_eval do
     key << "#{self.quantity}x"
     key << "<#{variant.cache_key}>"
     key << "@#{self.price}#{self.currency}"
-    key << "to<#{self.order.shipping_address.cache_key}>"
-  end  
+    key << "to<#{self.order.ship_address.try(:cache_key)}>"
+  end
 end
