47bfd43821cc82cdfbee80dec8a4fd2e28e860a1
Brian Christian <brchristian@gmail.com>
2014-07-19 19:22:21 -0600

    fall back to billing_address in line_item cache key if shipping_address is nil

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -5,6 +5,10 @@ Spree::LineItem.class_eval do
     key << "#{self.quantity}x"
     key << "<#{variant.cache_key}>"
     key << "@#{self.price}#{self.currency}"
-    key << "to<#{self.order.ship_address.try(:cache_key)}>"
+    if order.ship_address
+      key << "to<#{self.order.ship_address.try(:cache_key)}>"
+    elsif order.billing_address
+      key << "billed_to<#{self.order.billing_address.try(:cache_key)}>"
+    end
   end
 end
