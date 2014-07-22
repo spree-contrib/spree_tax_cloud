802137dd441e08a66c4d11d7845a088550a00b66
Matthew Fenelon <matt@fnln.me>
2014-07-22 14:39:54 +0100

    Make spree_tax_cloud play nice with other tax rates.
    This stops spree_tax_cloud committing tax cloud transactions for orders that do not use the tax cloud calculator. Stores that ship into and outside the US are likely to have multiple tax rates, with only the US tax rate being applicable for orders into the US.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,13 +1,25 @@
 Spree::Order.class_eval do
 
-  self.state_machine.after_transition to: :complete, do: :capture_tax_cloud  
+  self.state_machine.after_transition to: :complete, do: :capture_tax_cloud
 
   def capture_tax_cloud
+    if is_taxed_using_tax_cloud? == false
+      return
+    end
+
     transaction = Spree::TaxCloud.transaction_from_order(self)
-    response =  transaction.authorized_with_capture 
+    response =  transaction.authorized_with_capture
     if response != "OK"
       Rails.logger.error "ERROR: TaxCloud returned an order capture response of #{response}."
     end
   end
-  
+
+  def is_taxed_using_tax_cloud?
+    # TaxRate.match is used here to check if the order is taxable by Tax Cloud.
+    # It's not possible check against the order's tax adjustments because
+    # an adjustment is not created for 0% rates. However, US orders must be
+    # submitted to Tax Cloud even when the rate is 0%.
+    is_tax_cloud = Spree::TaxRate.match(tax_zone).any? { |rate| rate.calculator_type == "Spree::Calculator::TaxCloudCalculator" }
+    return is_tax_cloud
+  end
 end
