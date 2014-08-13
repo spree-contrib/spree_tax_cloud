14055b158922474bb18210d9b493b5c2d6f5d458
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-08-13 14:33:03 -0400

    Add product tic to permitted attributes.

--- lib/spree_tax_cloud/engine.rb
+++ lib/spree_tax_cloud/engine.rb
@@ -11,6 +11,10 @@ module SpreeTaxCloud
       g.test_framework :rspec
     end
 
+    initializer "spree_tax_cloud.permitted_attributes" do |app|
+      Spree::PermittedAttributes.product_attributes << :tax_cloud_tic
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
