315541a0430a12a59c26a34bfd116bc37b493013
MisinformedDNA <MisinformedDNA@users.noreply.github.com>
2014-08-27 21:37:34 +0000

    Remove hard dependency on spree_frontend

--- lib/spree_tax_cloud/engine.rb
+++ lib/spree_tax_cloud/engine.rb
@@ -19,7 +19,27 @@ module SpreeTaxCloud
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+
+      if Spree::Core::Engine.frontend_available?
+        Rails.application.config.assets.precompile += [
+          'lib/assets/javascripts/spree/frontend/spree_tax_cloud.js',
+          'lib/assets/stylesheets/spree/frontend/spree_tax_cloud.css'
+        ]
+        Dir.glob(File.join(File.dirname(__FILE__), "../../controllers/*/frontend/*_decorator*.rb")) do |c|
+          Rails.configuration.cache_classes ? require(c) : load(c)
+        end
+      end
     end
+
+    def self.frontend_available?
+      @@frontend_available ||= Rails::Application::Railties.engines.map{ |e| e.class.to_s }.include?('Spree::Frontend::Engine')
+    end
+
+    if self.frontend_available?
+      paths["app/controllers"] << "lib/controllers/frontend"
+      paths["app/views"] << "lib/views/frontend"
+    end
+
     config.to_prepare &method(:activate).to_proc
   end
 end
--- spree_tax_cloud.gemspec
+++ spree_tax_cloud.gemspec
@@ -17,11 +17,11 @@ Gem::Specification.new do |s|
   s.add_dependency 'spree_api'
   s.add_dependency 'spree_backend'
   s.add_dependency 'spree_core', '~> 2.3.1'
-  s.add_dependency 'spree_frontend'
 
   s.add_runtime_dependency 'savon', '~> 2.5.1'
   s.add_runtime_dependency 'tax_cloud', '~> 0.3.0'
 
+  s.add_development_dependency 'spree_frontend'
   s.add_development_dependency 'capybara'
   s.add_development_dependency 'poltergeist'
   s.add_development_dependency 'coffee-rails'
