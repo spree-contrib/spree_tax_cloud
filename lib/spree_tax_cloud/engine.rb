8f59cfb992da3f6a8cb9ca809d3f75ef42e72112
MisinformedDNA <MisinformedDNA@users.noreply.github.com>
2014-08-27 21:37:34 +0000

    Remove hard dependency on spree_frontend.
    Fixes #12

--- lib/spree_tax_cloud/engine.rb
+++ lib/spree_tax_cloud/engine.rb
@@ -16,10 +16,30 @@ module SpreeTaxCloud
     end
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/*/spree/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+
+      if SpreeTaxCloud::Engine.frontend_available?
+        Rails.application.config.assets.precompile += [
+          'lib/assets/javascripts/spree/frontend/spree_tax_cloud.js',
+          'lib/assets/stylesheets/spree/frontend/spree_tax_cloud.css'
+        ]
+        Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/frontend/*_decorator*.rb")) do |c|
+          Rails.configuration.cache_classes ? require(c) : load(c)
+        end
+      end
     end
+
+    def self.frontend_available?
+      @@frontend_available ||= ::Rails::Engine.subclasses.map(&:instance).map{ |e| e.class.to_s }.include?('Spree::Frontend::Engine')
+    end
+
+    if self.frontend_available?
+      paths["app/controllers"] << "lib/controllers/frontend"
+      paths["app/views"] << "lib/views/frontend"
+    end
+
     config.to_prepare &method(:activate).to_proc
   end
 end
