ba4535fc3869667e55be28f0b76530f53d66c8dd
MisinformedDNA <MisinformedDNA@users.noreply.github.com>
2014-08-28 04:09:20 +0000

    Engine updated so that the server can start

--- lib/spree_tax_cloud/engine.rb
+++ lib/spree_tax_cloud/engine.rb
@@ -20,7 +20,7 @@ module SpreeTaxCloud
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
 
-      if Spree::Core::Engine.frontend_available?
+      if SpreeTaxCloud::Engine.frontend_available?
         Rails.application.config.assets.precompile += [
           'lib/assets/javascripts/spree/frontend/spree_tax_cloud.js',
           'lib/assets/stylesheets/spree/frontend/spree_tax_cloud.css'
@@ -32,7 +32,7 @@ module SpreeTaxCloud
     end
 
     def self.frontend_available?
-      @@frontend_available ||= Rails::Application::Railties.engines.map{ |e| e.class.to_s }.include?('Spree::Frontend::Engine')
+      @@frontend_available ||= ::Rails::Engine.subclasses.map(&:instance).map{ |e| e.class.to_s }.include?('Spree::Frontend::Engine')
     end
 
     if self.frontend_available?
