15a72734dcc351f7d0800e19c5d999ce1dda5b7c
MisinformedDNA <MisinformedDNA@users.noreply.github.com>
2014-08-29 18:27:05 +0000

    Shortens the tax_cloud_cache_key.
    In my findings, the path length in a mkdir cannot exceed 245 characters. The length was being exceed on my system, so it needed to be shortened. Order key should be redundant with the remaining composite key.
    
    Fixes #13

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Shipment.class_eval do
   def tax_cloud_cache_key
-    "#{self.cache_key}--order:#{self.order.cache_key}--from:#{self.stock_location.cache_key}--to:#{self.order.shipping_address.cache_key}"
+    "#{self.cache_key}--from:#{self.stock_location.cache_key}--to:#{self.order.shipping_address.cache_key}"
   end  
 end
