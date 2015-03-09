0e1adccfd9a5565ce61a634e7a354707e309bb4c
Ben A Morgan <ben@benmorgan.io>
2015-03-09 15:26:23 -0700

    don't set tax cloud from spree

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -5,11 +5,5 @@ Spree::AppConfiguration.class_eval do
   preference :taxcloud_shipping_tic, :string, default: '11010'
   preference :taxcloud_usps_user_id, :string
   
-  TaxCloud.configure do |config|
-    config.api_login_id = Spree::Config.taxcloud_api_login_id
-    config.api_key = Spree::Config.taxcloud_api_key
-    config.usps_username = Spree::Config.taxcloud_usps_user_id
-  end
-  
   Rails.application.config.spree.calculators.tax_rates << Spree::Calculator::TaxCloudCalculator
 end
