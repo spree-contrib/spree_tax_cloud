05d11abf948b0c1ff1bed8154af3d77cec5f4957
Ben A Morgan <ben@benmorgan.io>
2015-03-09 13:37:45 -0700

    refactor tax cloud settings controller

--- app/controllers/spree/admin/tax_cloud_settings_controller.rb
+++ app/controllers/spree/admin/tax_cloud_settings_controller.rb
@@ -1,7 +1,6 @@
 module Spree
   module Admin
     class TaxCloudSettingsController < Spree::Admin::BaseController
-      
       def edit
         @preferences_login = [:taxcloud_api_login_id, :taxcloud_api_key, :taxcloud_usps_user_id]
         @preferences_tic = [:taxcloud_default_product_tic, :taxcloud_shipping_tic]
@@ -9,23 +8,20 @@ module Spree
 
       def update
         params.each do |name, value|
-          next unless Spree::Config.has_preference? name
-          Spree::Config[name] = value
+          Spree::Config[name] = value if Spree::Config.has_preference? name
         end
         flash[:success] = Spree.t(:successfully_updated, resource: Spree.t(:tax_cloud_settings))
-
         redirect_to edit_admin_tax_cloud_settings_path
       end
 
       def dismiss_alert
-        if request.xhr? and params[:alert_id]
+        if request.xhr? && params[:alert_id]
           dismissed = Spree::Config[:dismissed_spree_alerts] || ''
           Spree::Config.set dismissed_spree_alerts: dismissed.split(',').push(params[:alert_id]).join(',')
           filter_dismissed_alerts
           render nothing: true
         end
       end
-      
     end
   end
 end
