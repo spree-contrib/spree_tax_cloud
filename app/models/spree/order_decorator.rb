cd93069c1a5a5ab9554d1c2eee4c39db71391086
Ben A Morgan <ben@benmorgan.io>
2015-03-09 13:34:51 -0700

    refactor order decorator

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,27 +1,21 @@
 Spree::Order.class_eval do
-
   self.state_machine.after_transition to: :complete, do: :capture_tax_cloud
 
   def capture_tax_cloud
-    if is_taxed_using_tax_cloud? == false
-      return
-    end
-
-    transaction = Spree::TaxCloud.transaction_from_order(self)
-    response =  transaction.authorized_with_capture
+    return if is_taxed_using_tax_cloud? == false
+    response =  Spree::TaxCloud.transaction_from_order(self).authorized_with_capture
     if response != "OK"
       Rails.logger.error "ERROR: TaxCloud returned an order capture response of #{response}."
     end
   end
 
+  # TaxRate.match is used here to check if the order is taxable by Tax Cloud.
+  # It's not possible check against the order's tax adjustments because
+  # an adjustment is not created for 0% rates. However, US orders must be
+  # submitted to Tax Cloud even when the rate is 0%.
   def is_taxed_using_tax_cloud?
-    # TaxRate.match is used here to check if the order is taxable by Tax Cloud.
-    # It's not possible check against the order's tax adjustments because
-    # an adjustment is not created for 0% rates. However, US orders must be
-    # submitted to Tax Cloud even when the rate is 0%.
-    is_tax_cloud = Spree::TaxRate.match(self.tax_zone).any? do |rate|
+    Spree::TaxRate.match(self.tax_zone).any? do |rate|
       rate.calculator_type == "Spree::Calculator::TaxCloudCalculator"
     end
-    return is_tax_cloud
   end
 end
