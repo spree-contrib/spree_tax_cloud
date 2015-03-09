aee5874a3909edd27899298d612656c284ffb2de
Ben A Morgan <ben@benmorgan.io>
2015-03-09 16:03:00 -0700

    use symbol

--- lib/controllers/frontend/spree/checkout_controller_decorator.rb
+++ lib/controllers/frontend/spree/checkout_controller_decorator.rb
@@ -5,7 +5,7 @@ Spree::CheckoutController.class_eval do
   end
 
   rescue_from TaxCloud::Errors::ApiError do |exception|
-    flash[:error] = Spree.t("address_verification_failed")
+    flash[:error] = Spree.t(:address_verification_failed)
     redirect_to checkout_state_path(:address)
   end
 end
