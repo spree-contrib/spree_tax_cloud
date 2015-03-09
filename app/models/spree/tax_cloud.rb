3b7f44ae198dd4f10849e71b5dbd71cec6852be6
Ben A Morgan <ben@benmorgan.io>
2015-03-09 13:20:57 -0700

    use locals

--- app/models/spree/tax_cloud.rb
+++ app/models/spree/tax_cloud.rb
@@ -2,9 +2,7 @@ module Spree
   class TaxCloud
     def self.transaction_from_order(order)
       stock_location = order.shipments.first.try(:stock_location) || Spree::StockLocation.active.where("city IS NOT NULL and state_id IS NOT NULL").first
-      unless stock_location
-        raise 'Please ensure you have at least one Stock Location with a valid address for your tax origin.'
-      end
+      Spree.t(:ensure_one_valid_stock_location) unless stock_location
 
       transaction = ::TaxCloud::Transaction.new(
       customer_id: order.user_id || order.email,
