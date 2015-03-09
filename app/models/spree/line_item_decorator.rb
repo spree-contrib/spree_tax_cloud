401a4b80d8352a8e2b3f9ca91d2933829262433b
Ben A Morgan <ben@benmorgan.io>
2015-03-09 13:34:37 -0700

    stock concatenating sting and remove redundant selfs

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,14 +1,10 @@
 Spree::LineItem.class_eval do
   def tax_cloud_cache_key
-    key = "Spree::LineItem "
-    key << "#{self.id}: "
-    key << "#{self.quantity}x"
-    key << "<#{variant.cache_key}>"
-    key << "@#{self.price}#{self.currency}"
+    key = "Spree::LineItem #{id}: #{quantity}x<#{variant.cache_key}>@#{price}#{currency}"
     if order.ship_address
-      key << "to<#{self.order.ship_address.try(:cache_key)}>"
+      key << "shipped_to<#{order.ship_address.try(:cache_key)}>"
     elsif order.billing_address
-      key << "billed_to<#{self.order.billing_address.try(:cache_key)}>"
+      key << "billed_to<#{order.bill_address.try(:cache_key)}>"
     end
   end
 end
