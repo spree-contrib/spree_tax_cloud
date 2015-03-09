d7cfaf42cf65f3ebbf4b080e1777085c3d969c7e
Ben A Morgan <ben@benmorgan.io>
2015-03-09 13:23:18 -0700

    overrides should use the rails way
    This is a pet peeve. WHY do rails developers code their overrides like
    its PHP? Seriously? Anyone know what convention is??? Oh, wait, you’re
    a rails developer. Maybe instead of a PHP laundry list, there could be
    something a bit a more properly named with some folder structure? I
    don’t understand why laundry lists are so popular in overrides…

--- app/overrides/spree/admin/products/_form.rb
+++ app/overrides/spree/admin/products/_form.rb
@@ -1,7 +1,6 @@
 Deface::Override.new(
   virtual_path: "spree/admin/products/_form",
-  name: "add_tic_to_admin_product_edit",
+  name: "add tic to admin product edit",
   insert_after: "[data-hook='admin_product_form_tax_category']",
   partial: "spree/admin/products/edit_tax_cloud_tic"
 )
-
