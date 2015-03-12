44a9c322170043bb80c00fbc048879101a6f71f1
Ben A Morgan <ben@benmorgan.io>
2015-03-12 14:26:19 -0700

    properly log tax cloud errors

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,11 +3,12 @@ Spree::Order.class_eval do
 
   def capture_tax_cloud
     return unless is_taxed_using_tax_cloud?
-    response =  Spree::TaxCloud.transaction_from_order(self).authorized_with_capture
-    if response != "OK"
-      Rails.logger.error "ERROR: TaxCloud returned an order capture response of #{response}."
+    response = begin
+      Spree::TaxCloud.transaction_from_order(self).authorized_with_capture
+    rescue TaxCloud::Errors::ApiError => error
+      error.as_json.except("__better_errors_bindings_stack")
     end
-    log_tax_cloud(response)
+    log_tax_cloud response
   end
 
   # TaxRate.match is used here to check if the order is taxable by Tax Cloud.
