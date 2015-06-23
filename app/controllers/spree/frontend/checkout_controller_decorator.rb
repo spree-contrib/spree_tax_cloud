00473e50e0cb7a0a576dd9b67c63ff5539751886
JohnMorales <jmorales@gmail.com>
2015-06-23 13:14:00 -0400

    Fixing nameerror for Spree::CheckoutController

--- app/controllers/spree/frontend/checkout_controller_decorator.rb
+++ app/controllers/spree/frontend/checkout_controller_decorator.rb
@@ -1,11 +1,14 @@
-Spree::CheckoutController.class_eval do
-  rescue_from SpreeTaxCloud::Error do |exception|
-    flash[:error] = exception.message
-    redirect_to checkout_state_path(:address)
-  end
+# Currently the eager load paths with require every rb file in app/controllers
+if SpreeTaxCloud::Engine.frontend_available?
+  Spree::CheckoutController.class_eval do
+    rescue_from SpreeTaxCloud::Error do |exception|
+      flash[:error] = exception.message
+      redirect_to checkout_state_path(:address)
+    end
 
-  rescue_from TaxCloud::Errors::ApiError do |exception|
-    flash[:error] = Spree.t(:address_verification_failed)
-    redirect_to checkout_state_path(:address)
+    rescue_from TaxCloud::Errors::ApiError do |exception|
+      flash[:error] = Spree.t(:address_verification_failed)
+      redirect_to checkout_state_path(:address)
+    end
   end
 end
